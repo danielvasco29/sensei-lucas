a74962154960aa3cfbe20933444e36c3c9d159f3
danielvasco29 <danielvasconcelos.reis@gmail.com>
2022-09-16 05:54:29 -0300

    feat -> create service, repository, controller

---
+++ src/controllers/CreateUserController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+
+import { CreateUserService } from '../services/CreateUserService';
+
+class CreateUserController {
+  async control(req: Request, res: Response): Promise<Response> {
+    const userObject = req.body;
+
+    const createUserService = new CreateUserService();
+
+    const newUser = await createUserService.execute({ userData: userObject });
+
+    return res.status(201).json(newUser);
+  }
+}
+
+export { CreateUserController };
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import { hash } from 'bcrypt';
+
+import { UserEntity } from '../database/entities/UserEntity';
+import { UsersRepository } from '../database/repositories/UsersRepository';
+
+type CreateUserDTO = {
+  userData: UserEntity;
+};
+
+class CreateUserService {
+  async execute({ userData }: CreateUserDTO): Promise<UserEntity> {
+    const { email, password } = userData;
+    const usersRepository = new UsersRepository();
+
+    const userConflict = await usersRepository.findByEmail({ email });
+    if (userConflict) {
+      throw new Error('User already exists!');
+    }
+
+    const newPass = await hash(password, 10);
+    // eslint-disable-next-line no-param-reassign
+    userData.password = newPass;
+
+    const newUser = await usersRepository.create({ userData });
+    if (!newUser) {
+      throw new Error('User creation failed, contact suport for more details');
+    }
+
+    return newUser;
+  }
+}
+
+export { CreateUserService };
