35544c3db89a979dbac4979d18c94544e0eb83c8
danielvasco29 <danielvasconcelos.reis@gmail.com>
2022-09-16 04:53:06 -0300

    feat -> server config sucess

---
+++ src/shared/app.ts
@@ -0,0 +1,11 @@
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+export { app };
