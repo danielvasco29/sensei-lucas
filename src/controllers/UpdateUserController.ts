85f2d54cc486210a57c5854961ed4aa3f23947d3
danielvasco29 <danielvasconcelos.reis@gmail.com>
2022-09-17 08:11:13 -0300

    feat -> updateU user

--- src/controllers/UpdateUserController.ts
+++ src/controllers/UpdateUserController.ts
@@ -3,13 +3,16 @@ import { Request, Response } from 'express';
 import { UpdateUserUseService } from '../services/UpdateUserUseService';
 
 class UpdateUserController {
+  constructor(private updateUserUseService: UpdateUserUseService) {}
+
   async control(req: Request, res: Response): Promise<Response> {
     const { id } = req.body;
     const userData = req.body;
 
-    const updateUserUseService = new UpdateUserUseService();
-
-    const updateUser = await updateUserUseService.execute({ id, userData });
+    const updateUser = await this.updateUserUseService.execute({
+      id,
+      userData,
+    });
 
     return res.status(200).json(updateUser);
   }
