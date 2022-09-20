1bbfca69fd342177eb4994a8ce6653e29e8842a0
danielvasco29 <danielvasconcelos.reis@gmail.com>
2022-09-20 13:27:49 -0300

    doc -> initiate doc swagger

--- src/shared/app.ts
+++ src/shared/app.ts
@@ -1,16 +1,19 @@
-import cors from 'cors';
-import 'express-async-errors';
-import express from 'express';
-
 import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+
 import { errorHandle } from '../utils/errorHandle';
 import { router } from './routes';
+import swaggerDocument from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(router);
 app.use(errorHandle);
 
