a2d9b02a522c6091d5b00bc5ae08111b9a9ac0ca
danielvasco29 <danielvasconcelos.reis@gmail.com>
2022-11-03 12:36:01 -0300

    feat -> return book finished

--- src/modules/book/useCases/returnRentBook/ReturnRentController.ts
+++ src/modules/book/useCases/returnRentBook/ReturnRentController.ts
@@ -5,11 +5,11 @@ class ReturnRentController {
     async control(req: Request, res: Response): Promise<Response> {
         const userId = req.headers['x-user-id'] as string;
 
-        const bookstoreBooksId = req.headers['x-rentuserbook-id'] as string;
+        const rentBookUserId = req.headers['x-rentuserbook-id'] as string;
 
         const returnRentService = new ReturnRentService();
 
-        const returnBook = await returnRentService.execute({ userId, returnRent: bookstoreBooksId })
+        const returnBook = await returnRentService.execute({ userId, returnRent: rentBookUserId })
 
         return res.status(204).json(returnBook);
     }
